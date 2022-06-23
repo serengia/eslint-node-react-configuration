78826c248f8758dd23c8da6a784daa8662d36a71
James Serengia <jamesserengia@gmail.com>
2022-06-23 14:55:46 +0300

    Modified gitignore

---
+++ frontend/src/components/TestTitle.js
@@ -0,0 +1,16 @@
+import { Component, useEffect, useRef } from "react";
+
+const y = 10;
+
+// const TestTitle = function () {
+//   return <h2>TestTitle</h2>;
+// };
+// export default TestTitle;
+
+class TestTitle extends Component {
+  render() {
+    return <h2>Test Title two</h2>;
+  }
+}
+
+export default TestTitle;
